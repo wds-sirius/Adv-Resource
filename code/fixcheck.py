52ebe5d51fe0754e623d768f32634e3a59936d25
CPK <samkit157@yahoo.com>
2024-07-10 21:02:05 +0800

    測試用

---
+++ code/fixcheck.py
@@ -0,0 +1,14 @@
+import msgpack_lz4block
+import json
+import requests
+import os
+
+# get spine, background, bgm, card data from episode detail
+templist = json.load(open(os.path.join('./_temp', 'Temp.json'), 'rb'))
+backgroundlist = templist['background']
+bgmlist = templist['bgm']
+selist = templist['se']
+cardlist = templist['card']
+spinelist = templist['spine']
+
+print(templist)
