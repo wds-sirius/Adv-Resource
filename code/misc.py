7b3c9bbfabcc6fd3e0a8e2eab544644ee47ea196
CPaKit <samkit157@gmail.com>
2025-10-23 20:46:12 +0800

    add Update catalog file job in action

--- code/misc.py
+++ code/misc.py
@@ -10,67 +10,122 @@ WDS_Env = (WDS_Env_Req.json())['result']
 manifest_dir = './manifest'
 
 # spine setting
+spinePathIndex = -1
 spineFolder = './spine'
 spinemaster = json.load(open(os.path.join(manifest_dir, 'Spine.json'), 'rb'))
 if not os.path.exists(spineFolder):
     os.makedirs(spineFolder)
 
 # cards setting
+# cardsPathIndex = -1
 cardsFolder = './card'
 if not os.path.exists(cardsFolder):
     os.makedirs(cardsFolder)
 
 # background setting
+bgPathIndex = -1
 bgFolder = './background'
 bgmaster = json.load(open(os.path.join(manifest_dir, 'Background.json'), 'rb'))
 if not os.path.exists(bgFolder):
     os.makedirs(bgFolder)
 
-# load temp list
+# # load temp list
 tempList = json.load(open(os.path.join('./_temp', 'Temp.json'), 'rb'))
 
-############# spine
-for spineId in tempList['spine'][:]:
-    # check is file not exist
-    isExist = [item for item in spinemaster if item.get('Id') == int(spineId)]
-    IsExitImg = False
-    IsExitMeta = False
-    if len(isExist) == 0:
-        try:
-            fullurl = f'{WDS_Env["assetUrl"]}/2d-assets/Android/{WDS_Env["assetVersion"]}/adventurecharacterstand_assets_adventurecharacterstands/{spineId}.prefab.bundle'
-            assetsReq = requests.get(fullurl)
-            if assetsReq.status_code == 200:
-                assetsbundle = UnityPy.load(assetsReq.content)
-                for obj in assetsbundle.objects:
-                    if obj.type.name == "Texture2D":
-                        data = obj.read()
-                        data.image.save(os.path.join(spineFolder, f'{spineId}.png'))
-                        IsExitImg = True
+# load catalog
+catalog_json = json.load(open(os.path.join('./_temp', '2dcatalog.json'), 'rb'))
 
-                    if obj.type.name == "TextAsset":
-                        data = obj.read()
-                        ext = data.name.split('.')[-1]
-                        open(os.path.join(spineFolder, f'{spineId}.{ext}'), "wb").write(bytes(data.script))
-                        IsExitMeta = True
+# get pathId from catalog
+for index, value in enumerate(catalog_json['m_InternalIdPrefixes']):
+    if value.endswith('adventurecharacterstand_assets_adventurecharacterstands'):
+        spinePathIndex = index
+    # elif value.endswith('charactercards_assets_charactercard'):
+    #     cardsPathIndex = index
+    elif value.endswith('adventurebackground_assets_adventurebackgrounds'):
+        bgPathIndex = index
 
-                if IsExitImg and IsExitMeta:
-                    tempList['spine'].remove(spineId)
-                    spinemaster.append({
-                        "Id" : int(spineId),
-                        "CharacterId" : int(spineId[0 : 3]),
-                        "CompanyId" : int(spineId[0]),
-                    })
-        except:
-            print(spineId)
-    else:
-        tempList['spine'].remove(spineId)
+
+for asset in catalog_json['m_InternalIds']:
+
+    # spine
+    if asset.startswith(f'{spinePathIndex}#'):
+        filename = asset.split('/')[-1]
+        spineId = filename.split('.')[0]
+
+        # check the spine data isexit
+        isExist = [item for item in spinemaster if item.get('Id') == int(spineId)]
+        IsExitImg = False
+        IsExitMeta = False
+        if len(isExist) == 0:
+            try:
+                fullurl = f'{WDS_Env["assetUrl"]}/2d-assets/Android/{WDS_Env["assetVersion"]}/adventurecharacterstand_assets_adventurecharacterstands/{spineId}.prefab.bundle'
+                assetsReq = requests.get(fullurl)
+                if assetsReq.status_code == 200:
+                    assetsbundle = UnityPy.load(assetsReq.content)
+                    for obj in assetsbundle.objects:
+                        if obj.type.name == "Texture2D":
+                            data = obj.read()
+                            data.image.save(os.path.join(spineFolder, f'{spineId}.png'))
+                            IsExitImg = True
+
+                        if obj.type.name == "TextAsset":
+                            data = obj.read()
+                            ext = data.name.split('.')[-1]
+                            open(os.path.join(spineFolder, f'{spineId}.{ext}'), "wb").write(bytes(data.script))
+                            IsExitMeta = True
+
+                    if IsExitImg and IsExitMeta:
+                        if spineId in tempList['spine']:
+                            tempList['spine'].remove(spineId)
+                        spinemaster.append({
+                            "Id" : int(spineId),
+                            "CharacterId" : int(spineId[0 : 3]),
+                            "CompanyId" : int(spineId[0]),
+                        })
+            except:
+                print(spineId)
+        else:
+            if spineId in tempList['spine']:
+                tempList['spine'].remove(spineId)
+    
+    # backgorund
+    if asset.startswith(f'{bgPathIndex}#'):
+        filename = asset.split('/')[-1]
+        bgId = filename.split('.')[0]
+
+        if bgId not in bgmaster:
+            try:
+                fullurl = f'{WDS_Env["assetUrl"]}/2d-assets/Android/{WDS_Env["assetVersion"]}/adventurebackground_assets_adventurebackgrounds/{bgId}.bundle'
+                assetsReq = requests.get(fullurl)
+                if assetsReq.status_code == 200:
+                    assetsbundle = UnityPy.load(assetsReq.content)
+                    for obj in assetsbundle.objects:
+                        if obj.type.name == "Texture2D":
+                            data = obj.read()
+                            if data.name == bgId:
+                                data.image.save(os.path.join(bgFolder, f'{data.name}.png'))
+                                bgmaster.append(data.name)
+                                if bgId in tempList['background']:
+                                    tempList['background'].remove(bgId)
+            except:
+                print(bgId)
+        else:
+            if bgId in tempList['background']:
+                tempList['background'].remove(bgId)
+
 
 # save spine list
 spinemaster.sort(key = lambda x: x["Id"] )
 json_data = json.dumps(spinemaster, indent=4, ensure_ascii=False)
 open(os.path.join(manifest_dir, 'Spine.json'), "w", encoding='utf8').write(json_data)
 
-############# card
+# save background list
+bgmaster.sort()
+bg_json_data = json.dumps(bgmaster, indent=4, ensure_ascii=False)
+open(os.path.join(manifest_dir, 'Background.json'), "w", encoding='utf8').write(bg_json_data)
+
+
+# card
 for cardId in tempList['card'][:]:
     try:
         fullurl = f'{WDS_Env["assetUrl"]}/2d-assets/Android/{WDS_Env["assetVersion"]}/charactercards_assets_charactercard/{cardId}.bundle'
@@ -86,31 +141,6 @@ for cardId in tempList['card'][:]:
                         tempList['card'].remove(cardId)
     except:
         print(cardId)
-
-############# backgorund
-for bgId in tempList['background'][:]:
-    if bgId not in bgmaster:
-        try:
-            fullurl = f'{WDS_Env["assetUrl"]}/2d-assets/Android/{WDS_Env["assetVersion"]}/adventurebackground_assets_adventurebackgrounds/{bgId}.bundle'
-            assetsReq = requests.get(fullurl)
-            if assetsReq.status_code == 200:
-                assetsbundle = UnityPy.load(assetsReq.content)
-                for obj in assetsbundle.objects:
-                    if obj.type.name == "Texture2D":
-                        data = obj.read()
-                        if data.name == bgId:
-                            data.image.save(os.path.join(bgFolder, f'{data.name}.png'))
-                            bgmaster.append(data.name)
-                            tempList['background'].remove(bgId)
-        except:
-            print(bgId)
-    else:
-        tempList['background'].remove(bgId)
-
-# save background list
-bgmaster.sort()
-bg_json_data = json.dumps(bgmaster, indent=4, ensure_ascii=False)
-open(os.path.join(manifest_dir, 'Background.json'), "w", encoding='utf8').write(bg_json_data)
 
 ############ save temp json
 tempList_data = json.dumps(tempList, indent=4, ensure_ascii=False)
